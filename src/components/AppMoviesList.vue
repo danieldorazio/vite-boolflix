28e6ae504d38004d3b6752116f4f66fbffc2c193
danieldorazio <danieldorazio.97@gmail.com>
2023-11-30 12:38:42 +0100

    added the cover of the film or series to our list. if it has not added image not found

--- src/components/AppMoviesList.vue
+++ src/components/AppMoviesList.vue
@@ -24,6 +24,9 @@ export default {
 
 <template>
     <section class="my_MoviesList">
+        <div> 
+             <img :src="`https://image.tmdb.org/t/p/w185${arrayMovies.poster_path}`" alt="" onerror="this.onerror=null; this.src='https://png.pngtree.com/png-vector/20221125/ourmid/pngtree-no-image-available-icon-flatvector-illustration-pic-design-profile-vector-png-image_40966566.jpg';"> <!--..\assets\img\img-not-found.jpg* -->
+        </div>
         <div>{{ arrayMovies.title }}</div>
         <div>{{ arrayMovies.original_title }}</div>
         <div>
--- src/components/AppTvList.vue
+++ src/components/AppTvList.vue
@@ -24,6 +24,9 @@ export default {
 
 <template>
     <section class="my_TvList">
+        <div> 
+             <img :src="`https://image.tmdb.org/t/p/w185${arrayTv.poster_path}`" alt="" onerror="this.onerror=null; this.src='https://png.pngtree.com/png-vector/20221125/ourmid/pngtree-no-image-available-icon-flatvector-illustration-pic-design-profile-vector-png-image_40966566.jpg';"> <!--..\assets\img\img-not-found.jpg* -->
+        </div>
         <div>{{ arrayTv.name }}</div>
         <div>{{ arrayTv.original_name }}</div>
         <div>
